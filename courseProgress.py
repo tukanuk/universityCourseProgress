0167b4ee94d6865cfe1e3b60eb85e4cb83ca8541
Ben Davidson <benjamin@idavidson.ca>
2021-03-12 20:09:55 -0500

    supressed verbose output by default

--- courseProgress.py
+++ courseProgress.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
-# import json
-# import requests
 import csv
 import argparse
 import datetime
-# import time
-# from shutil import copyfile
 
 class Activity:
     """ An Activity Record """
@@ -18,8 +14,6 @@ class Activity:
         self.email = email
         self.progress = float(progress)/100
         self.complete = (complete == "TRUE")
-
-        # self.lastAccesed = lastAccesed
         self.lastAccesed = datetime.datetime.strptime(lastAccesed, '%Y-%m-%d')
 
 
@@ -30,66 +24,47 @@ Name:     {self.name:25} Email:    {self.email:20}
 Progress: {self.progress * 100:>3.0f}{"%":22} Complete: {self.complete}
 Accesed:  {self.lastAccesed.date()}\n""")
 
-def sortActivities(activityList):
-    # dateList = [activity.lastAccesed for activity in activityList]
-    # dateList.sort()
-    # print(dateList)
-
-    sortedList = sorted(activityList, key=lambda activity : activity.lastAccesed)
-    for s in sortedList:
-        print(s.lastAccesed)
-
-
 def main():
     print("")
 
     #   ./courseProgress.py -f team.csv -c "Course" -o result.csv 
     parser = argparse.ArgumentParser(description="Track the team progress of a university course")
     parser.add_argument("dtuActivityFile", nargs=1, metavar="<dtu_activity_file>", help="The name of the .csv file")
-    parser.add_argument("-course", "-c", help="The name of the course you are interested in")
-    parser.add_argument("-output", "-o", required=False, help="Specifiy the custom file output, otherwise _result will be appended")
-
+    parser.add_argument("--course", "-c", help="The name of the course you are interested in")
+    parser.add_argument("--output", "-o", required=False, help="Specifiy the custom file output, otherwise _result will be appended")
+    parser.add_argument("-v", "--verbose", help="Verbose output", action='store_true')
 
     args = parser.parse_args()
 
-    print(f"{'The args are:':35} {args}")
-
+    # print(f"{'The args are:':35} {args}")
 
     # extract the args
     dtuActivityFile = args.dtuActivityFile[0]
     course = args.course
+    verbose = args.verbose
 
     if args.output != None:
         outputFileName = args.output
     else:
         outputFileName = f"{dtuActivityFile.split('.')[0]}_result.csv"
 
-    print(f"{'The activity file is:':35} {dtuActivityFile}\n{'The course we are interested in is:':35} '{course}'\n{'The output will be:':35} {outputFileName}")
-
+    if verbose: print(f"{'The activity file is:':35} {dtuActivityFile}\n{'The course we are interested in is:':35} '{course}'\n{'The output will be:':35} {outputFileName}")\
 
     dtuActivityList = []
 
     # open the dtu_activity file
     with open(dtuActivityFile, newline='', encoding='utf-8-sig') as csvfile:
         data = csv.DictReader(csvfile)
-        print(f"{'Field check:':35} {data.fieldnames}")
+        if verbose: print(f"{'Field check:':35} {data.fieldnames}")
         for row in data:
             # print(f"Course: {row['asset']}\t Name: {row['name']}\t Progress: {row['progress']}\t Last Accessed: {row['lastAccess']}")
             activity = Activity(row['type'], row['asset'], row['name'], row['email'], row['progress'], row['complete'], row['lastAccess'])
             # activity.printInfo()
             dtuActivityList.append(activity)
 
-
-
-    # test the list
-    # for item in dtuActivityList:
-    #     item.printInfo()
-
     # filter the list
     if course != None:
         dtuActivityList = [act for act in dtuActivityList if act.courseName == course]
-        # for act in dtuActivityList:
-        #     print(act.printInfo())
     else:
         print("No filter applied")
 
@@ -99,7 +74,7 @@ def main():
 
     print()
     for d in dtuActivityList:
-        d.printInfo()
+        if verbose: d.printInfo()
     
 
     with open(outputFileName, mode="w", encoding="utf=8") as csvwriter:
