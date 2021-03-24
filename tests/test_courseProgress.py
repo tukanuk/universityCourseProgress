acc943852b53e8b4e293cb1b1c62708d3b9cf34e
benjamin <benjamin@idavidson.ca>
2021-03-23 21:49:59 -0400

    added requirements.txt

--- tests/test_courseProgress.py
+++ tests/test_courseProgress.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 from courseProgress import courseProgress
 
 
 def createActivityObject():
     activity = courseProgress.Activity(
-        "course", "Course Name", "Ben User", "user@email.com", 0.9, False, "3/22/21"
+        "course", "Course Name", "Ben User", "user@email.com", 0.9, "false", "3/22/21"
     )
 
     return activity
@@ -13,3 +14,5 @@ def test_createActivityObject():
 
     activity = createActivityObject()
     assert activity.courseName == "Course Name"
+    assert activity.complete == False
+    assert datetime.strftime(activity.lastAccesed, '%m/%d/%y') == "03/22/21"
